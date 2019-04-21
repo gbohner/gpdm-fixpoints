e0b637b0f413a8062000049416ddd85bbea0a851
Gergo Bohner <gbohner@gatsby.ucl.ac.uk>
2019-04-21 19:33:47 +0100

    Update simulations with nullclines in the firing rate state space

--- thesis_Machens_Brody_sim/thesis_create_ExperimentData_spiking.m
+++ thesis_Machens_Brody_sim/thesis_create_ExperimentData_spiking.m
@@ -2,9 +2,9 @@ addpath(genpath('twonode'))
 
 cur_sim_time = datestr(now,30);
 
-show_plots = 0;
+show_plots = 1;
 
-nTrials = 1000;
+nTrials = 10 ;
 
 % Fix the random seed for reproducibility
 rng(2718);
@@ -26,7 +26,7 @@ for i = 1:nTrials
   
   if show_plots
     % Time evolution of gsx and gsy
-    scatter(gsx, gsy, 5, 1:numel(gsx)); colormap(jet(numel(gsx))); colorbar
+    scatter(data(i).gsx, data(i).gsy, 5, 1:numel(data(i).gsx)); colormap(jet(numel(data(i).gsx))); colorbar
   end
   
   if mod(i,50)==0
@@ -35,8 +35,12 @@ for i = 1:nTrials
   
 end
 
-save(['thesis_MachensBrodySim_' cur_sim_time '_loading_uneven.mat'], 'data', 'nullcline1', 'nullcline2', 'par')
+load('fr_from_inhib_load.mat') % Loads fr_from_inhib_x and fr_from_inhib_f
+[nullcline1_fr, nullcline2_fr] = transform_nullclines_to_fr(...
+      nullcline1,nullcline2, fr_from_inhib_x, fr_from_inhib_f);
 
+save(['thesis_MachensBrodySim_' cur_sim_time '_loading_uneven.mat'], 'data', ...
+    'nullcline1', 'nullcline2', 'nullcline1_fr', 'nullcline2_fr', 'par')
 if show_plots
   plot(nullcline1(1,:)*1e-3, nullcline1(2,:)*1e-3);
   plot(nullcline2(1,:)*1e-3, nullcline2(2,:)*1e-3);
@@ -61,7 +65,7 @@ for i = 1:nTrials
   
   if show_plots
     % Time evolution of gsx and gsy
-    scatter(gsx, gsy, 5, 1:numel(gsx)); colormap(jet(numel(gsx))); colorbar
+    scatter(data(i).gsx, data(i).gsy, 5, 1:numel(data(i).gsx)); colormap(jet(numel(data(i).gsx))); colorbar
   end
   
   if mod(i,50)==0
@@ -70,8 +74,12 @@ for i = 1:nTrials
   
 end
 
-save(['thesis_MachensBrodySim_' cur_sim_time '_loading_even.mat'], 'data', 'nullcline1', 'nullcline2', 'par')
+load('fr_from_inhib_load.mat') % Loads fr_from_inhib_x and fr_from_inhib_f
+[nullcline1_fr, nullcline2_fr] = transform_nullclines_to_fr(...
+      nullcline1,nullcline2, fr_from_inhib_x, fr_from_inhib_f);
 
+save(['thesis_MachensBrodySim_' cur_sim_time '_loading_even.mat'], 'data', ...
+    'nullcline1', 'nullcline2', 'nullcline1_fr', 'nullcline2_fr', 'par')
 if show_plots
   plot(nullcline1(1,:)*1e-3, nullcline1(2,:)*1e-3);
   plot(nullcline2(1,:)*1e-3, nullcline2(2,:)*1e-3);
@@ -96,7 +104,8 @@ for i = 1:nTrials
   
   if show_plots
     % Time evolution of gsx and gsy
-    scatter(gsx, gsy, 5, 1:numel(gsx)); colormap(jet(numel(gsx))); colorbar
+    scatter(data(i).gsx, data(i).gsy, 5, 1:numel(data(i).gsx)); colormap(jet(numel(data(i).gsx))); colorbar
+    %scatter(data(i).xx, data(i).yy, 5, 1:numel(data(i).xx)); colormap(jet(numel(data(i).xx))); colorbar
   end
   
   if mod(i,50)==0
@@ -105,9 +114,28 @@ for i = 1:nTrials
   
 end
 
-save(['thesis_MachensBrodySim_' cur_sim_time '_decision.mat'], 'data', 'nullcline1', 'nullcline2', 'par')
+load('fr_from_inhib_deci.mat') % Loads fr_from_inhib_x and fr_from_inhib_f
+[nullcline1_fr, nullcline2_fr] = transform_nullclines_to_fr(...
+      nullcline1,nullcline2, fr_from_inhib_x, fr_from_inhib_f);
+
+save(['thesis_MachensBrodySim_' cur_sim_time '_decision.mat'], 'data', ...
+    'nullcline1', 'nullcline2', 'nullcline1_fr', 'nullcline2_fr', 'par')
 
 if show_plots
   plot(nullcline1(1,:)*1e-3, nullcline1(2,:)*1e-3);
   plot(nullcline2(1,:)*1e-3, nullcline2(2,:)*1e-3);
 end
+
+[nullcline1_fr, nullcline2_fr] = transform_nullclines_to_fr(...
+      nullcline1,nullcline2, par.x, fr_from_inhib_deci);
+figure; plot(nullcline1_fr(1,:), nullcline1_fr(2,:));
+hold on;
+plot(nullcline2_fr(1,:), nullcline2_fr(2,:));
+
+[nullcline1_fr, nullcline2_fr] = transform_nullclines_to_fr(...
+      nullcline1,nullcline2, par.x, fr_from_inhib_load);
+figure; plot(nullcline1_fr(1,:), nullcline1_fr(2,:));
+hold on;
+plot(nullcline2_fr(1,:), nullcline2_fr(2,:));
+
+figure; plot(par.x, fr_from_inhib_deci)
